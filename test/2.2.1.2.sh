f34657ad298df4d53695ffd65e0bbc3abf01ec93
Lammert Hellinga <l.hellinga@quintor.nl>
2019-08-08 14:40:59 +0200

    Update ntp checks
    Either ntp or chrony should be installed and configured
    both checks now determine if the other is installed and
    exit gracefully

--- test/2.2.1.2.sh
+++ test/2.2.1.2.sh
@@ -2,11 +2,20 @@
 # ** AUTO GENERATED **
 
 # 2.2.1.2 - Ensure ntp is configured (Scored)
-
 rpm -q ntp | grep -E "^ntp-"
-if [[ $? -eq 0 ]]; then
+app1=$?
+rpm -q chrony | grep -E "^chrony-"
+app2=$?
 
-  grep "^restrict -6 default kod nomodify notrap nopeer noquery" /etc/ntp.conf || exit $1
+if [[ $app1 -eq 1 && $app2 -eq 1 ]]; then
+    # no ntp solution installed
+    exit 1
+elif [[ $app1 -eq 1 && $app2 -eq 0 ]]; then
+    # chrony installed, no use checking ntp config
+    exit 0
+else
+    # check ntp config
+    grep "^restrict -6 default kod nomodify notrap nopeer noquery" /etc/ntp.conf || exit $1
 
-  grep -E "^(server|pool)" /etc/ntp.conf || exit $1
+    grep -E "^(server|pool)" /etc/ntp.conf || exit $1
 fi
--- test/2.2.1.3.sh
+++ test/2.2.1.3.sh
@@ -2,9 +2,19 @@
 # ** AUTO GENERATED **
 
 # 2.2.1.3 - Ensure chrony is configured (Scored)
-
+rpm -q ntp | grep -E "^ntp-"
+app1=$?
 rpm -q chrony | grep -E "^chrony-"
-if [[ $? -eq 0 ]]; then
-        grep -E "^(server|pool)" /etc/chrony.conf || exit $?
-        grep -E "^OPTIONS" /etc/sysconfig/chronyd | grep "OPTIONS=\"-u chrony\"" || exit $?
+app2=$?
+
+if [[ $app1 -eq 1 && $app2 -eq 1 ]]; then
+    # no ntp installed
+    exit 1
+elif [[ $app1 -eq 0 && $app2 -eq 1 ]]; then
+    # ntp installed and not chrony, no use in checking
+    exit 0
+else
+    # chrony installed, check config
+    grep -E "^(server|pool)" /etc/chrony.conf || exit $?
+    grep -E "^OPTIONS" /etc/sysconfig/chronyd | grep "OPTIONS=\"-u chrony\"" || exit $?
 fi
