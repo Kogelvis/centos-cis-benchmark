717877a52f92e2113a7d5fc729966b964f653be5
Lammert Hellinga <l.hellinga@quintor.nl>
2019-08-08 10:51:25 +0200

    Update check 1.7.1.4

--- test/1.7.1.4.sh
+++ test/1.7.1.4.sh
@@ -3,4 +3,8 @@
 
 # 1.7.1.4 - Ensure permissions on /etc/motd are configured (Not Scored)
 
-stat -L -c "%a %u %g" /etc/motd | grep -q "644 0 0$" || exit $?
+# check user/group
+stat -L -c "%u %g" /etc/motd | grep -q "0 0$" || exit $?
+
+# check that /etc/motd is min 444 and max 644 in rights
+find /etc/motd -perm -444 ! -perm /133 | grep -q "/etc/motd" || exit $?
